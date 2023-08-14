a36bd8011f67ef3edaaeeb20a99ca7b44424a0d6
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2023-08-14 20:48:04 +0300

    Loop a calculator

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String s;
+        double answer;
 
-        int a = getInputValue(in, "Enter value a: ");
-        int b = getInputValue(in, "Enter value b: ");
+        while(true) {
+            int a = getInputValue(in, "Enter value a: ");
+            int b = getInputValue(in, "Enter value b: ");
 
-        System.out.println("Enter an option ( * / + - ): ");
-        String s = in.nextLine();
+            System.out.println("Enter an option ( * / + - ): ");
+            s = in.nextLine();
 
-        double answer = 0.0;
+            answer = 0.0;
 
-        switch (s) {
-            case "*":
-                answer = a * b;
-                break;
-            case "/":
-                if (b == 0) {
-                    System.out.println("Value cannot be divided by zero");
+            switch (s) {
+                case "*":
+                    answer = a * b;
+                    break;
+                case "/":
+                    if (b == 0) {
+                        System.out.println("Value cannot be divided by zero");
+                        System.exit(0);
+                    } else {
+                        answer = (double) a / b;
+                    }
+                    break;
+                case "+":
+                    answer = a + b;
+                    break;
+                case "-":
+                    answer = a - b;
+                    break;
+                default:
+                    System.out.println("Wrong command");
                     System.exit(0);
-                } else {
-                    answer = (double) a / b;
-                }
-                break;
-            case "+":
-                answer = a + b;
-                break;
-            case "-":
-                answer = a - b;
-                break;
-            default:
-                System.out.println("Wrong command");
-                System.exit(0);
-                break;
+                    break;
+            }
+            DecimalFormat decimalFormat = new DecimalFormat("#.###");
+            System.out.printf("Answer is: %s", decimalFormat.format(answer));
+            System.out.println();
         }
-        DecimalFormat decimalFormat = new DecimalFormat("#.###");
-        System.out.printf("Answer is: %s", decimalFormat.format(answer));
     }
 
     private static int getInputValue(Scanner scanner, String prompt) {
