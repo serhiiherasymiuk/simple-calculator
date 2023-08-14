a5b23b7fce027532f8cccdb3c480580281607605
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2023-08-14 19:33:53 +0300

    Added calculator

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,45 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter value a: ");
+        int a;
+        a = Integer.parseInt(in.nextLine());
+        System.out.println("Enter value b: ");
+        int b;
+        b = Integer.parseInt(in.nextLine());
+        System.out.println("Enter an option (* / + -): ");
+        String s;
+        s = in.nextLine();
+        int answer = 0;
+        switch (s) {
+            case ("*"):
+                answer = a * b;
+                break;
+            case ("/"):
+                if (b == 0) {
+                    System.out.println("Value cannot be divided by zero");
+                    System.exit(0);
+                }
+                else
+                    answer = a / b;
+                break;
+            case ("+"):
+                answer = a + b;
+                break;
+            case ("-"):
+                answer = a - b;
+                break;
+            default:
+                System.out.println("Error");
+                break;
         }
+        System.out.printf("Answer is: %d", answer);
+
     }
 }
